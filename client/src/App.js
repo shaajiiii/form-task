e04ec978cfdbe62877e503859b0dd16b0040b582
Manu <manushaji717@gmail.com>
2023-01-13 12:49:58 +0530

    Created client

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Form from './components/Form';
+function App() {
+  return (
+    <div className="App">
+      <h1>Test header inside app</h1>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Form.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Form() {
+  return (
+    <div style={{ backgroundColor: "skyblue" }}>
+      <span>span text </span>
+    </div>
+  )
+}
+
+export default Form
