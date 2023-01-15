fdd3348122202139b7c439f8c4e5000f62e93119
Manu <manushaji717@gmail.com>
2023-01-15 06:48:27 +0530

    States set for form fields. Validations next

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -1,44 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Form.module.css';
 
 function Form() {
+
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [mobile, setMobile] = useState('');
+  const [age, setAge] = useState('');
+  const [dob, setDob] = useState('');
+
+
+  let handleSubmit = (e)=>{
+    e.preventDefault();
+    let formData = {
+      firstName: firstName,
+      lastName: lastName,
+      mobile: mobile,
+      age: age,
+      email: email,
+      dob: dob
+    }
+    console.log(formData);
+  }
+
   return (
     <div className={`${styles.form_container} p-4 mt-4`} >
       <h1>Form</h1>
-      <form className='row'>
+      <form className='row' onSubmit={handleSubmit}>
+
         <div class="col-12 col-md-6 mb-2">
           <label for="validationCustom01" class="form-label">First name</label>
-          <input type="text" class="form-control" value="Test name" required />
+          <input type="text" class="form-control"
+            value={firstName}
+            onChange={(e) => { setFirstName(e.target.value) }}
+            required />
           <div class="valid-feedback">
             Looks good!
           </div>
         </div>
+
         <div class="col-12 col-md-6 ">
           <label for="validationCustom02" class="form-label">Last name</label>
-          <input type="text" class="form-control" value="test lname" required />
+          <input type="text" class="form-control"
+            value={lastName}
+            onChange={(e) => { setLastName(e.target.value) }}
+            required />
           <div class="valid-feedback">
             Looks good!
           </div>
         </div>
+
         <div class="mb-2">
           <label for="exampleInputEmail1" class="form-label">Email address</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+          <input type="email" class="form-control"
+            value={email}
+            onChange={(e) => { setEmail(e.target.value) }}
+            aria-describedby="emailHelp" />
           {/* <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> */}
         </div>
 
         <div class="col-md-4">
           <label class="form-label">Mobile</label>
-          <input type="number" class="form-control" required />
+          <input type="number" class="form-control"
+            value={mobile}
+            onChange={(e) => { setMobile(e.target.value) }}
+            required />
         </div>
 
         <div class="col-6 col-md-4">
           <label class="form-label">Age</label>
-          <input type="number" class="form-control" required />
+          <input type="number" class="form-control"
+            value={age}
+            onChange={(e) => { setAge(e.target.value) }}
+            required />
         </div>
 
         <div class="col-6 col-md-4 mb-4">
           <label class="form-label">DOB</label>
-          <input type="date" class="form-control" required />
+          <input type="date" class="form-control"
+            value={dob}
+            onChange={(e) => { setDob(e.target.value) }}
+            required />
         </div>
 
 
